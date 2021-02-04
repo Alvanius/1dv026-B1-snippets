fd2e9b8dbbf5ca72c6df8470db16c5a4b1177e55
Alva Persson <ap223st@student.lnu.se>
2021-02-04 14:47:28 +0100

    Create express app w basic setup and start server

--- src/server.js
+++ src/server.js
@@ -5,19 +5,62 @@
  * @version 1.0.0
  */
 
+import express from 'express'
+import hbs from 'express-hbs'
+import logger from 'morgan'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 import { connectDB } from './config/mongoose.js'
 
 /**
  * The main function of  the application.
  */
 const main = async () => {
+  //  Connect to the database.
   try {
     await connectDB()
   } catch (error) {
     console.log(error.message)
     process.exitCode = 1
+    // If no database connection can be established there´s no point in starting up the server.
     return
   }
+
+  const PORT = process.env.PORT || 5000
+  const directoryFullName = dirname(fileURLToPath(import.meta.url))
+
+  // Create an express application.
+  const app = express()
+
+  // ------------------------------------
+  //          Middlewares section
+  // ------------------------------------
+  app.use(logger('dev'))
+
+  app.set('view engine', 'hbs')
+
+  // Configurations of the view engine.
+  app.engine('hbs', hbs.express4({
+    defaultLayout: join(directoryFullName, 'views', 'layouts', 'default'),
+    partialsDir: join(directoryFullName, 'views', 'partials')
+  }))
+  app.set('views', join(directoryFullName, 'views'))
+
+  // Parse requests of the content type application/x-www-form-urlencoded.
+  // Populates the request object with a body object (req.body).
+  app.use(express.urlencoded({ extended: false }))
+
+  // Serve static files from the public directory.
+  app.use(express.static(join(directoryFullName, '..', 'public')))
+  // ------------------------------------
+  //      End of Middlewares section
+  // ------------------------------------
+
+  // Starts the HTTP server.
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+    console.log('Press Ctrl-C to terminate...')
+  })
 }
 
 main()
