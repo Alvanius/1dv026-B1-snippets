e8cf96318ad01468991709672a81add009cf80ad
Alva Persson <ap223st@student.lnu.se>
2021-02-17 10:39:57 +0100

    Add use of helmet and remove unneccessary comments

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@
 
 import express from 'express'
 import session from 'express-session'
+import helmet from 'helmet'
 import hbs from 'express-hbs'
 import logger from 'morgan'
 import { dirname, join } from 'path'
@@ -18,7 +19,6 @@ import { connectDB } from './config/mongoose.js'
  * The main function of  the application.
  */
 const main = async () => {
-  //  Connect to the database.
   try {
     await connectDB()
   } catch (error) {
@@ -29,15 +29,14 @@ const main = async () => {
 
   const PORT = process.env.PORT || 5000
   const directoryFullName = dirname(fileURLToPath(import.meta.url))
-
-  // Create an express application.
   const app = express()
 
   // ------------------------------------
   //         Middleware section
   // ------------------------------------
   app.use(logger('dev'))
-
+  app.use(helmet())
+  app.set('trust proxy', 1)
   app.set('view engine', 'hbs')
 
   // Configurations of the view engine.
@@ -47,34 +46,30 @@ const main = async () => {
   }))
   app.set('views', join(directoryFullName, 'views'))
 
-  // Parse requests of the content type application/x-www-form-urlencoded.
-  // Populates the request object with a body object (req.body).
   app.use(express.urlencoded({ extended: false }))
-
-  // Serve static files from the public directory.
   app.use(express.static(join(directoryFullName, '..', 'public')))
 
-  // Setup and use session middleware (https://github.com/expressjs/session)
+  // Setup for session middleware express-session
   const sessionOptions = {
     name: 'snippetApp',
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 1000 * 60 * 60 * 24 // 1 day
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24,
+      sameSite: 'lax'
     }
   }
-
-  /*  app.set('trust proxy', 1) */
 
   app.use(session(sessionOptions))
 
   app.use(function (req, res, next) {
+    // Flash messages and delete indication only survives for a round trip
     if (req.session.flash) {
       res.locals.flash = req.session.flash
       delete req.session.flash
     }
-
     if (req.session.delete) {
       res.locals.delete = req.session.delete
       delete req.session.delete
@@ -84,10 +79,8 @@ const main = async () => {
     next()
   })
 
-  // Register routes.
   app.use('/', router)
 
-  // Error handler.
   app.use(function (err, req, res, next) {
     let errortext = 'Sorry, but something went wrong.'
     let error = '500 Internal server error'
@@ -110,7 +103,6 @@ const main = async () => {
   //      End of Middleware section
   // ------------------------------------
 
-  // Starts the HTTP server.
   app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
     console.log('Press Ctrl-C to terminate...')
